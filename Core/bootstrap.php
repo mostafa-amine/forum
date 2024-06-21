42639faf15653c18ad4037257dabcc1a2a9a4e13
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2024-06-21 15:00:46 +0100

    bind validator

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -3,11 +3,16 @@
 use Core\App;
 use Core\Container;
 use Core\Database;
+use Core\Validator;
 
 $container = new Container();
 
 $container->bind(Database::class, function () {
     return new Database(config('database'));
+});
+
+$container->bind(Validator::class, function () {
+    return new Validator();
 });
 
 App::setContainer($container);
